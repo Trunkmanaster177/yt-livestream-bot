6d856cf658f35edcb266a22db5d9dd63234af343
Trunkmanaster177 <newmusic177@gmail.com>
2025-06-03 15:57:32 +0530

    main.py

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
-import subprocess
+import os
+import time
 
-# Image file name
-image_file = 'your_image.png'
+STREAM_KEY = os.getenv("0u91-thf0-2yca-3x17-3f39")  # use env var for security
 
-# Your YouTube stream key (hardcoded)
-stream_key = '0u91-thf0-2yca-3x17-3f39'
-
-# FFmpeg command to loop image and stream to YouTube
-ffmpeg_command = [
-    'ffmpeg',
-    '-loop', '1',
-    '-i', image_file,
-    '-c:v', 'libx264',
-    '-tune', 'stillimage',
-    '-preset', 'veryfast',
-    '-b:v', '2500k',
-    '-maxrate', '2500k',
-    '-bufsize', '5000k',
-    '-pix_fmt', 'yuv420p',
-    '-g', '50',
-    '-c:a', 'aac',
-    '-b:a', '128k',
-    '-ar', '44100',
-    '-f', 'flv',
-    f'rtmp://a.rtmp.youtube.com/live2/{stream_key}'
-]
-
-# Run the command
-subprocess.run(ffmpeg_command)
+while True:
+    # FFmpeg command to loop an image forever
+    os.system(f"""
+        ffmpeg -re -loop 1 -i your_image.png \
+        -f lavfi -i anullsrc \
+        -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k \
+        -pix_fmt yuv420p -c:a aac -b:a 128k -g 50 \
+        -shortest -f flv rtmp://a.rtmp.youtube.com/live2/{STREAM_KEY}
+    """)
+    
+    # Optional: wait before restarting if FFmpeg stops
+    time.sleep(5)
